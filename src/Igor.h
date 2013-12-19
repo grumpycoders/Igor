6d81f3bb428a42a3c016172d9a4c02deaaa2947c
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2013-12-19 15:42:53 -0800

    Forgot Igor.h...

---
+++ src/Igor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Exceptions.h>
+#include <Handle.h>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+typedef Balau::IO<Balau::Handle> BFile;

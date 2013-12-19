f6c6d2fd3b4f6906dfa84f86c9039415c8d45902
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2013-12-18 23:59:59 -0800

    Enabling a few more modules.

--- Igor.cpp
+++ Igor.cpp
@@ -1,7 +1,10 @@
 #include <Main.h>
+#include <Printer.h>
+#include <Input.h>
+#include <BStream.h>
 
 using namespace Balau;
 
 void MainTask::Do() {
-
+    Printer::log(M_STATUS, "Igor starting up");
 }

e08724a51cf36b3e9ad25aeb15aa92ccaafe509b
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2013-12-21 23:50:33 -0800

    Test of sending data via WebSockets.

--- src/IgorHttp.cpp
+++ src/IgorHttp.cpp
@@ -1,17 +1,18 @@
 #include <HttpServer.h>
 #include <Task.h>
 #include <SimpleMustache.h>
+#include <BWebSocket.h>
 
 using namespace Balau;
 
 const char htmlTemplateStr[] =
-"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n"
-"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
-"<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+"<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN'\n"
+"'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>\n"
+"<html xmlns='http://www.w3.org/1999/xhtml'>\n"
 "  <head>\n"
-"    <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />\n"
+"    <meta http-equiv='content-type' content='text/html; charset=utf-8' />\n"
 "    <title>{{title}}</title>\n"
-"    <style type=\"text/css\">\n"
+"    <style type='text/css'>\n"
 "        body { font-family: arial, helvetica, sans-serif; }\n"
 "    </style>\n"
 "  </head>\n"
@@ -19,23 +20,24 @@ const char htmlTemplateStr[] =
 "  <body>\n"
 "    <h1>{{title}}</h1>\n"
 "    <h2>{{msg}}</h2>\n"
-"    <div id=\"resultDiv\"></div>\n"
-"    <input type=\"button\" id=\"textButton\" value=\"Load\"></input>\n"
-"    <script src=\"//ajax.googleapis.com/ajax/libs/dojo/1.9.2/dojo/dojo.js\" data-dojo-config=\"has:{'dojo-firebug': true}, async: true\"></script>\n"
+"    <div id='resultDiv'></div>\n"
+"    <script src='//ajax.googleapis.com/ajax/libs/dojo/1.9.2/dojo/dojo.js' data-dojo-config='has:{\"dojo-firebug\": true}, async: true'></script>\n"
 "    <script>\n"
-"      require([\"dojo/dom\", \"dojo/on\", \"dojo/request\", \"dojo/domReady!\"], function(dom, on, request) {\n"
-"        var resultDiv = dom.byId(\"resultDiv\");\n"
+"      require(['dojo/dom', 'dojo/on', 'dojo/request', 'dojox/socket', 'dojox/socket/Reconnect', 'dojo/domReady!'], function(dom, on, request) {\n"
+"        var resultDiv = dom.byId('resultDiv');\n"
+"        var socket = dojox.socket({url:'/igorws'});\n"
+"        socket = dojox.socket.Reconnect(socket);\n"
 "\n"
-"        on(dom.byId(\"textButton\"), \"click\", function(evt) {\n"
-"          request.get(\"test.txt\").then(\n"
-"            function(text) {\n"
-"              resultDiv.innerHTML = text;\n"
-"            },\n"
-"            function(error) {\n"
-"              resultDiv.innerHTML = error;\n"
-"            }\n"
-"          );\n"
+"        dojo.connect(socket, 'onopen', function(event) {\n"
+"          resultDiv.innerHTML = 'opened';\n"
+"          socket.send('hi there');\n"
 "        });\n"
+"\n"
+"        dojo.connect(socket, 'onmessage', function(event) {\n"
+"          var message = event.data;\n"
+"          resultDiv.innerHTML = message;\n"
+"        });\n"
+"\n"
 "      });\n"
 "    </script>\n"
 "  </body>\n"
@@ -75,7 +77,7 @@ bool RootAction::Do(HttpServer * server, Http::Request & req, HttpServer::Action
     HttpServer::Response response(server, req, out);
 
     ctx["title"] = "Test";
-    ctx["msg"] = "This is a Dojo Ajax test.";
+    ctx["msg"] = "This is a Dojo WebSocket test.";
 
     testHtmlTemplate.htmlTemplate.render(response.get(), &ctx);
     response.Flush();
@@ -100,11 +102,31 @@ bool TestAction::Do(HttpServer * server, Http::Request & req, HttpServer::Action
     return true;
 }
 
+static Regex igorWSURL("/igorws$");
+
+class IgorWSWorker : public WebSocketWorker {
+public:
+    virtual void receiveMessage(const uint8_t * msg, size_t len, bool binary) {
+        if (binary)
+            Printer::log(M_INFO, "got binary message");
+        else
+            Printer::log(M_INFO, "got text message '%s'", msg);
+        WebSocketFrame * frame = new WebSocketFrame(String("Hello World!"), 1);
+        sendFrame(frame);
+    }
+    IgorWSWorker(IO<Handle> socket, const String & url) : WebSocketWorker(socket, url) { }
+};
+
+class IgorWSAction : public WebSocketServer<IgorWSWorker> {
+public:
+    IgorWSAction() : WebSocketServer(igorWSURL) { }
+};
+
 void igor_setup_httpserver() {
     HttpServer * s = new HttpServer();
     s->registerAction(new RootAction());
     s->registerAction(new TestAction());
+    s->registerAction(new IgorWSAction());
     s->setPort(8080);
-    s->setLocal("localhost");
     s->start();
 }

46df7abc0c7264cf12caf54cfa44f835660f5b92
Vincent Hamm <vincent.hamm@gmail.com>
2013-12-27 16:14:32 -0800

    Split wxWidgets build scripts

--- wxWidgets_x64.bat
+++ wxWidgets_x64.bat
@@ -2,6 +2,4 @@ cd wxWidget/build/msw
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 nmake.exe /f makefile.vc CPU=x64 BUILD=release MONOLITHIC=1 DEBUG_INFO=1
 nmake.exe /f makefile.vc CPU=x64 BUILD=debug MONOLITHIC=1 DEBUG_INFO=1
-nmake.exe /f makefile.vc CPU=x86 BUILD=release MONOLITHIC=1 DEBUG_INFO=1
-nmake.exe /f makefile.vc CPU=x86 BUILD=debug MONOLITHIC=1 DEBUG_INFO=1
 cd ../../..

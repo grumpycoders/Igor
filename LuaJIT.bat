2707e52d2a1a966d33d20dc753e4c63a0458910e
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2014-05-25 00:08:43 -0700

    Adding Lua support.

--- LuaJIT.bat
+++ LuaJIT.bat
@@ -3,11 +3,15 @@
 cd Balau\LuaJIT\src
 
 call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" x86
-call msvcbuild.bat static
-copy lua51.lib ..\..\..\lua51_32.lib
+call ..\..\..\msvcbuild.bat static
+copy lua51.lib ..\..\..\lua51_32r.lib
+call ..\..\..\msvcbuild.bat debug static
+copy lua51.lib ..\..\..\lua51_32d.lib
 
 call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" x86_amd64
-call msvcbuild.bat static
-copy lua51.lib ..\..\..\lua51_64.lib
+call ..\..\..\msvcbuild.bat static
+copy lua51.lib ..\..\..\lua51_64r.lib
+call ..\..\..\msvcbuild.bat debug static
+copy lua51.lib ..\..\..\lua51_64d.lib
 
 cd ..\..\..
